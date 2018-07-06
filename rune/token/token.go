4401f6b2476b7f0dbf2f1bb3dd26a64d4b3afb69
Kota Saito <noissefnoc@gmail.com>
2018-07-06 11:58:25 +0900

    first import

---
+++ rune/token/token.go
@@ -0,0 +1,77 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// identifier + literal
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT = "INT"     // 1323456
+
+	// operator
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	BANG = "!"
+	ASTERISK = "*"
+	SLASH = "/"
+
+	LT = "<"
+	GT = ">"
+
+	EQ = "=="
+	NOT_EQ = "!="
+
+	// delimiter
+	COMMA = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+
+	STRING = "STRING"
+
+	LBRACKET = "["
+	RBRACKET = "]"
+
+	COLON = ":"
+
+	MACRO = "MACRO"
+)
+
+var keywords = map[string]TokenType{
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+	"macro": MACRO,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
