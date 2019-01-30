1eb4f409a72612adee46310d56e6e51e28da2cd8
noissefnoc <noissefnoc@gmail.com>
2019-01-30 22:53:15 +0900

    add test for http client.

---
+++ test/apiclient/client.go
@@ -0,0 +1,29 @@
+package apiclient
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+type API struct {
+	Client *http.Client
+	baseURL string
+}
+
+func (api *API) DoStuff() ([]byte, error) {
+	resp, err := api.Client.Get(api.baseURL + "/some/path")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return body, err
+}
