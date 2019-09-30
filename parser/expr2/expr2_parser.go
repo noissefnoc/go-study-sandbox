f22ead1b646f704b822b8ecf501073ea60609fb5
noissefnoc <noissefnoc@gmail.com>
2019-09-30 23:53:48 +0900

    add example of expr2

---
+++ parser/expr2/expr2_parser.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"github.com/alecthomas/kong"
+	"github.com/alecthomas/participle"
+	"github.com/alecthomas/repr"
+	"strings"
+)
+
+// Based on http://www.craftinginterpreters.com/parsing-expressions.html
+
+// expression     → equality ;
+// equality       → comparison ( ( "!=" | "==" ) comparison )* ;
+// comparison     → addition ( ( ">" | ">=" | "<" | "<=" ) addition )* ;
+// addition       → multiplication ( ( "-" | "+" ) multiplication )* ;
+// multiplication → unary ( ( "/" | "*" ) unary )* ;
+// unary          → ( "!" | "-" ) unary
+//                | primary ;
+// primary        → NUMBER | STRING | "false" | "true" | "nil"
+//                | "(" expression ")" ;
+
+type Expression struct {
+	Equality *Equality `@@`
+}
+
+type Equality struct {
+	Comparison *Comparison `@@`
+	Op         string      `[ @( "!" "=" | "=" "=" )`
+	Next       *Equality   `  @@ ]`
+}
+
+type Comparison struct {
+	Addition *Addition   `@@`
+	Op       string      `[ @( ">" | ">" "=" | "<" | "<" "=" )`
+	Next     *Comparison `  @@ ]`
+}
+
+type Addition struct {
+	Multiplication *Multiplication `@@`
+	Op             string          `[ @( "-" | "+" )`
+	Next           *Addition       `  @@ ]`
+}
+
+type Multiplication struct {
+	Unary *Unary          `@@`
+	Op    string          `[ @( "/" | "*" )`
+	Next  *Multiplication `  @@ ]`
+}
+
+type Unary struct {
+	Op      string   `  ( @( "!" | "-" )`
+	Unary   *Unary   `    @@ )`
+	Primary *Primary `| @@`
+}
+
+type Primary struct {
+	Number        *float64    `  @Float | @Int`
+	String        *string     `| @String`
+	Bool          *bool       `| ( @"true" | "false" )`
+	Nil           bool        `| @"nil"`
+	SubExpression *Expression `| "(" @@ ")" `
+}
+
+func main() {
+	var cli struct {
+		Expr []string `arg required help:"Expression to parse."`
+	}
+	ctx := kong.Parse(&cli)
+	p := participle.MustBuild(&Expression{}, participle.UseLookahead(2))
+
+	expr := &Expression{}
+	err := p.ParseString(strings.Join(cli.Expr, " "), expr)
+	ctx.FatalIfErrorf(err)
+
+	repr.Println(expr)
+}
