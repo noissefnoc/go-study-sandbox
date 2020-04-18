3c107fc899b92dbf99d5247265f41a5ce2619837
noissefnoc <noissefnoc@gmail.com>
2020-04-18 22:12:23 +0900

    Add ignore fields options

--- go-cmp/README.md
+++ go-cmp/README.md
@@ -1,3 +1,4 @@
 # go-cmp
 
 [google/go-cmp](https://github.com/google/go-cmp) の調査用のディレクトリ。
+
--- go-cmp/go-cmp_test.go
+++ go-cmp/go-cmp_test.go
@@ -12,6 +12,17 @@ type Person struct {
 	Age  int
 }
 
+type Hoge struct {
+	Moji          string
+	AnotherStruct *Huga
+	Num           int
+	Flag          bool
+}
+
+type Huga struct {
+	Moji string
+}
+
 func TestGoCmp(t *testing.T) {
 	p1 := &Person{
 		Name: "Alice",
@@ -26,12 +37,42 @@ func TestGoCmp(t *testing.T) {
 	checkResult(t, p1, p2)
 }
 
+func TestNestedStructure(t *testing.T) {
+	h1 := &Hoge{
+		Moji: "hoge",
+		AnotherStruct: &Huga{
+			Moji: "huga",
+		},
+		Num:  1,
+		Flag: false,
+	}
+
+	h2 := &Hoge{
+		Moji: "hogehoge",
+		AnotherStruct: &Huga{
+			Moji: "hugahoge",
+		},
+		Num:  2,
+		Flag: false,
+	}
+
+	checkResult(t, h1, h2)
+}
+
 func checkResult(t *testing.T, expected, actual interface{}) {
 	t.Helper()
-
-	ignoreField := cmpopts.IgnoreFields(Person{}, "Name")
-
-	if diff := cmp.Diff(expected, actual, ignoreField); diff != "" {
+	opts := buildCmpOptions(t)
+	if diff := cmp.Diff(expected, actual, opts...); diff != "" {
 		t.Errorf("Mismatch (-want +got)\n%s", diff)
 	}
+}
+
+func buildCmpOptions(t *testing.T) []cmp.Option {
+	t.Helper()
+	opts := []cmp.Option{
+		cmpopts.IgnoreFields(Person{}, "Name"),
+		cmpopts.IgnoreFields(Huga{}, "Moji"),
+		cmpopts.IgnoreFields(Hoge{}, "Num", "Moji"),
+	}
+	return opts
 }
